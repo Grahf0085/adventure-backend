e15dae00c8727eb9927d0e480dd0dc89937eb832
Tucker <tuckerhoog@tutanota.com>
2021-08-05 21:10:47 -0400

    get all test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import { Adventurer } from '../lib/models/Adventurer';
 
 describe('adventurer routes', () => {
   beforeEach(() => {
@@ -44,5 +45,46 @@ describe('adventurer routes', () => {
       missionRank: 5,
       zone: 'Great Desert'
     });
+
   });
+
+  it('gets all adventurers', async () => {
+    
+    const newAdventurer = await Adventurer.create({
+      alias: 'Xeno',
+      currentClass: 'Warrior',
+      level: 75,
+      title: 'Heroe',
+      warLvl: 37,
+      mnkLvl: 16,
+      thfLvl: 37,
+      wizLvl: 0,
+      clrLvl: 0,
+      brdLvl: 0,
+      missionRank: 5,
+      zone: 'Great Desert'
+    });
+
+    const newAdventurerTwo = await Adventurer.create({
+      alias: 'Miang',
+      currentClass: 'Thief',
+      level: 77,
+      title: 'Unifier of Everyone',
+      warLvl: 0,
+      mnkLvl: 0,
+      thfLvl: 0,
+      wizLvl: 100,
+      clrLvl: 0,
+      brdLvl: 0,
+      missionRank: 10,
+      zone: 'Sky'
+    });
+
+    const res = await request(app)
+      .get('/api/v1/adv');
+
+    expect(res.body).toEqual([newAdventurer, newAdventurerTwo]);
+
+  });
+
 });
